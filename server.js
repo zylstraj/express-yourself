c1f356701b4454ac6c026382804cab607180e219
KMSkelton <kmskelton@outlook.com>
2015-07-06 22:13:07 -0700

    includes work, styling does not.

--- server.js
+++ server.js
@@ -10,13 +10,21 @@ app.set('views', path.join(__dirname, 'views'));
 //view engine setup
 app.set('view engine', 'jade');
 
-//index get request
+//index get requests
 app.get('/', function(req, res){
   res.render('index');
 });
-
 app.get('/about', function(req, res){
   res.render('about');
+});
+app.get('/footer', function(req, res){
+  res.render('footer');
+});
+app.get('/head', function(req, res){
+  res.render('head');
+});
+app.get('/nav', function(req, res){
+  res.render('nav');
 });
 
 //static services
