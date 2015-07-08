7e0cdfd8787db167a75ea535b71690e7857693e0
KMSkelton <kmskelton@outlook.com>
2015-07-08 10:38:52 -0700

    new endpoints. can use either res.render or res.send

--- server.js
+++ server.js
@@ -27,6 +27,27 @@ app.get('/nav', function(req, res){
   res.render('nav');
 });
 
+//REST handlers:
+// respond with "Hello World!" on the homepage
+app.get('/', function (req, res) {
+  res.send('CUBS WIN!!!');
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+// app.put('/user', function (req, res) {
+//   res.send('Got a PUT request at /user');
+// });
+
+// accept DELETE request at /user
+// app.delete('/user', function (req, res) {
+//   res.send('Got a DELETE request at /user');
+// });,
+
 //static services
 app.use(express.static(path.join(__dirname, 'public')));
 
